14473155ad96c2dcbe7a46f19ab4caa38148cb4b
Vince Yuan <vince.yuan@gmail.com>
2013-07-12 16:46:19 +0800

    Loaded string from index.html

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var content = fs.readFileSync(__dirname + '/index.html', 'utf8');
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
